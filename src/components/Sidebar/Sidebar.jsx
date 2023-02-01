5cea626cd549b8333153c4f962763f245dcedbbf
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-02-01 21:52:14 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/components/Sidebar/Sidebar.jsx 	new file:   src/components/SidebarOption/SidebarOption.css

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import TwitterIcon from '@mui/icons-material/Twitter';
+import SidebarOption from '../SidebarOption/SidebarOption';
+import "./Sidebar.css";
+
+
 
 const Sidebar = () => {
   return (
-    <div>sidebar
-    {/*Twitter icon */}
-    
-    {/*Sidebar Option */}
-    {/*Sidebar Option */}
-    {/*Sidebar Option */}
-    {/*Sidebar Option */}
-    {/*Sidebar Option */}
-    {/*Sidebar Option */}
+    <div className="sidebar">
+      <TwitterIcon/>
+      <SidebarOption/>
+      <SidebarOption/>
+      <SidebarOption/>
+      <SidebarOption/>
+      {/*Sidebar Option */}
+      {/*Sidebar Option */}
+      {/*Sidebar Option */}
+      {/*Sidebar Option */}
+      {/*Sidebar Option */}
+      {/*Sidebar Option */}
 
-    {/*Button -> Tweet*/}
+      {/*Button -> Tweet*/}
     </div>
 
   )
